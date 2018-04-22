ea2f88a30aa9477a9c23ed1465bb0200bb06d47e
yonig <yonigev@gmail.com>
2018-04-22 13:53:32 +0300

    a

--- proc.c
+++ proc.c
@@ -534,7 +534,7 @@ wakeup1(void *chan) {
 
     for (p = ptable.proc; p < &ptable.proc[NPROC]; p++){
         if(p->chan == chan){
-            if(!cas(&(p->state),SLEEPING,-RUNNABLE)){    //if not sleeping TODO: maybe not necessary?
+            if(!cas(&(p->state),SLEEPING,RUNNABLE)){    //if not sleeping TODO: maybe not necessary?
                 //while(p->state == -SLEEPING){}               //busy wait while -sleeping
                 //cas(&(p->state),-SLEEPING,RUNNABLE);         //when finally sleeping-wake it up
             }
